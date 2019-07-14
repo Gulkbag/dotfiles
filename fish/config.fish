1dd10c0e6d30d6b255595b17f747f71ad0d29ac5
Gulkbag <ahmednasser321432@gmail.com>
2019-07-14 18:58:38 +0200

    gruvbox/fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,5 @@ abbr --add dev 		cd ~/dev/
 abbr --add i3rc 	nvim ~/.config/i3/config
 abbr --add rrc 		nvim ~/.config/ranger/rc.conf
 abbr --add comprc 	nvim ~/.config/compton.conf
+
+set -g theme_color_scheme gruvbox

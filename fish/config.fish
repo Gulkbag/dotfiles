91b4389643866ead2441c9284375bfba91fa4721
Gulkbag <ahmednasser321432@gmail.com>
2019-07-24 19:22:29 +0200

    Update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -15,18 +15,20 @@ abbr --add surf     tabbed -c surf -e
 function fish_prompt
   set_color normal
   echo -n '['
-  set_color d79921
+  set_color fabd2f
   echo -n $USER
   set_color normal
   echo -n ' >> '
-  set_color 98971a
+  set_color b8bb26
   echo -n (prompt_pwd)
   set_color normal
   echo -n ' >> '
-  set_color cc241d
+  set_color fb4934
   echo -n " "(git branch 2>/dev/null | grep '^*' | colrm 1 2)
   set_color normal
-  echo -n '] '
+  echo -n ']'
+  set_color 8ec07c
+  printf '\n\t>>>> '
 end
 
 function fish_right_prompt
@@ -34,5 +36,5 @@ function fish_right_prompt
   date
 end
 
-set -g theme_color_scheme gruvbox
 set -x -U GOPATH $HOME/go
+set -x -U EDITOR /usr/bin/nvim

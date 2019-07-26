fe5babb29496c061e125c859014eb183cb969735
Gulkbag <ahmednasser321432@gmail.com>
2019-07-26 18:36:15 +0200

    Update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -17,10 +17,10 @@ abbr --add vim  nvim # I sometimes type vim instead of nvim
 abbr --add df   cd ~/dev/dotfiles/
 abbr --add dp   sh deploy.sh
 abbr --add xi   sudo xbps-install -Sy
-abbr --add xr   sudo xbps-remove -R
+abbr --add xr   sudo xbps-remove -Ry
 abbr --add xq   xbps-query -Rs
-abbr --add rmc  sudo xbps-remove -Oo # Removes all cached stuff
-abbr --add upd  sudo xbps-install -Su # Update
+abbr --add rmc  sudo xbps-remove -Ooy # Removes all cached stuff
+abbr --add upd  sudo xbps-install -Suy # Update
 
 function fish_prompt
   set_color normal
